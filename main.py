b359ea49901d58b22873620225f730b020d49698
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-03-04 09:20:31 +0500

    Commit

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT ,
     name VARCHAR(255) ,
     surname VARCHAR(255) ,
-    email TEXT NULL ,
+    email TEXT NULL UNIQUE,
     phone INTEGER)''')
-
-connection.execute("""INSERT INTO users(name, surname, email, phone)
-VALUES ("Ahmadjon", "Abdulfotih", "hmadjonbdul@gmail.com", 770333308)""")
 
 connection.commit()
 connection.close()
